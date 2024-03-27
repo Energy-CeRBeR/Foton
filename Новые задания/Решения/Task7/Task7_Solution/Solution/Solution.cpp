9c49f01c35bd0b63277899b32611f98d5161c517
Igor Chesnykh <igor.chesnyx@mail.ru>
2024-03-28 00:16:21 +0300

    Create the 7 task

--- Новые задания/Решения/Task7/Task7_Solution/Solution/Solution.cpp
+++ Новые задания/Решения/Task7/Task7_Solution/Solution/Solution.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <chrono>
 
 
 #pragma pack(2)
@@ -29,6 +30,77 @@ struct BITMAPINFOHEADER {
 };
 
 
+void thinning_image(const std::string INPUT_PATH, const std::string OUTPUT_PATH, int n) {
+    std::ifstream input_file(INPUT_PATH, std::ios::binary);
+    std::ofstream output_file(OUTPUT_PATH, std::ios::binary);
+
+    if (!(input_file.is_open() && output_file.is_open())) {
+        std::cout << "Can't open one of the files";
+        input_file.close();
+        output_file.close();
+        exit(1);
+    }
+
+    BITMAPFILEHEADER fileHeader;
+    input_file.read((char*)&fileHeader, sizeof(fileHeader));
+
+    BITMAPINFOHEADER fileInfoHeader;
+    input_file.read((char*)&fileInfoHeader, sizeof(fileInfoHeader));
+
+    int pixelsOffset = fileHeader.bfOffBits;
+    int colorsChannels = fileInfoHeader.biBitCount / 8;
+    int width = fileInfoHeader.biWidth;
+    int height = fileInfoHeader.biHeight;
+    int row_size = std::floor((fileInfoHeader.biBitCount * width + 31) / 32) * 4;
+
+    int new_width = width / n;
+    int new_height = height / n;
+    int new_row_size = std::floor((fileInfoHeader.biBitCount * new_width + 31) / 32) * 4;
+
+    fileInfoHeader.biWidth = new_width;
+    fileInfoHeader.biHeight = new_height;
+
+    output_file.write((char*)&fileHeader, sizeof(fileHeader));
+    output_file.write((char*)&fileInfoHeader, sizeof(fileInfoHeader));
+
+    char* temp_info = new char[pixelsOffset - 54];
+    input_file.read(temp_info, pixelsOffset - 54);
+    output_file.write(temp_info, pixelsOffset - 54);
+    delete[] temp_info;
+
+    auto startTime = std::chrono::high_resolution_clock::now();
+
+    std::vector<char> row(row_size);
+    int x = 0;
+    std::vector<char> new_pixels(new_row_size * new_height + (new_width + 1) * colorsChannels);
+    int count = 0;
+    for (int i = n / 2; i < height; i += n) {
+        int y = 0;
+        input_file.seekg(pixelsOffset + (height <= 0 ? (height - i - 1) : i) * row_size);
+        input_file.read(row.data(), row_size);
+
+        for (int j = n / 2; j < width; j += n) {
+            for (int k = 0; k < colorsChannels; k++) {
+                new_pixels[x * new_row_size + y * colorsChannels + k] = row[j * colorsChannels + k];
+            }
+            new_pixels[x * new_row_size + y * colorsChannels] = row[j * colorsChannels];
+            y++;
+        }
+        x++;
+    }
+
+    output_file.write(new_pixels.data(), new_row_size * new_height + new_width * colorsChannels);
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> duration = endTime - startTime;
+    std::cout << "New image has been successfully created using thinning for " << duration.count() << " seconds" << std::endl;
+
+    input_file.close();
+    output_file.close();
+}
+
+
 void averaging_image(const std::string INPUT_PATH, const std::string OUTPUT_PATH, int n) {
     std::ifstream input_file(INPUT_PATH, std::ios::binary);
     std::ofstream output_file(OUTPUT_PATH, std::ios::binary);
@@ -46,7 +118,7 @@ void averaging_image(const std::string INPUT_PATH, const std::string OUTPUT_PATH
     BITMAPINFOHEADER fileInfoHeader;
     input_file.read((char*)&fileInfoHeader, sizeof(fileInfoHeader));
 
-    long pixelsOffset = 54 + fileHeader.bfOffBits - sizeof(BITMAPFILEHEADER) - sizeof(BITMAPINFOHEADER);
+    int pixelsOffset = fileHeader.bfOffBits;
     int colorsChannels = fileInfoHeader.biBitCount / 8;
     int width = fileInfoHeader.biWidth;
     int height = fileInfoHeader.biHeight;
@@ -58,8 +130,7 @@ void averaging_image(const std::string INPUT_PATH, const std::string OUTPUT_PATH
 
     fileInfoHeader.biWidth = new_width;
     fileInfoHeader.biHeight = new_height;
-    fileInfoHeader.biSizeImage = new_width * colorsChannels + (new_height - 1) * new_row_size;
-    
+
     output_file.write((char*)&fileHeader, sizeof(fileHeader));
     output_file.write((char*)&fileInfoHeader, sizeof(fileInfoHeader));
 
@@ -68,53 +139,57 @@ void averaging_image(const std::string INPUT_PATH, const std::string OUTPUT_PATH
     output_file.write(temp_info, pixelsOffset - 54);
     delete[] temp_info;
 
-    std::vector<char> new_row(new_row_size);
+    auto startTime = std::chrono::high_resolution_clock::now();
+
     std::vector<char> row(row_size);
-    std::cout << "YEEEEE" << std::endl;
-    int x = 0;
-    for (int i = n / 2; i < height; i += n) {
-        x++;
-        int y = 0;
-        input_file.seekg(pixelsOffset + (height <= 0 ? (height - i - 1) : i) * row_size);
-        input_file.read(new_row.data(), new_row_size);
-        input_file.seekg(pixelsOffset + (height <= 0 ? (height - i - 1) : i) * row_size);
-        input_file.read(row.data(), row_size);
-
-        for (int j = n / 2; j < width; j += n) {
-            y++;
-            new_row[y * colorsChannels] = row[j * colorsChannels];
-            // Скорее всего он не успеват записать все пиксели!!!
-
-            /*char* pixel = new char[colorsChannels];
-            input_file.read(pixel, colorsChannels);
-            output_file.write(pixel, colorsChannels);
-            delete[] pixel;*/
-
-            //row[j * colorsChannels] = 255 - row[j * colorsChannels];
-            //new_row[x * colorsChannels] = row[j * colorsChannels];
+    std::vector<char> new_pixels(new_row_size * new_height + new_width * colorsChannels);
+    std::vector<char> pixels(row_size * height + width * colorsChannels);
+    input_file.read(pixels.data(), row_size * height + width * colorsChannels);
+    int x_1 = 0;
+    for (int x = 0; x <= height - n; x += n) {
+        int y_1 = 0;
+        for (int y = 0; y <= width - n; y += n) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    for (int k = 0; k < colorsChannels; k++) {
+                        new_pixels[x_1 * new_row_size + y_1 * colorsChannels + k] += (pixels[(x + i) * row_size + (y + j) * colorsChannels + k] / (n * n));
+                    }
+                }
+            }
+            y_1++;
         }
-        output_file.write(new_row.data(), new_row_size);
+        x_1++;
     }
     
-   
-    std::cout << "The negative image has been successfully created!" << std::endl;
+
+    output_file.write(new_pixels.data(), new_row_size * new_height + new_width * colorsChannels);
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> duration = endTime - startTime;
+    std::cout << "New image has been successfully created using averaging for " << duration.count() << " seconds" << std::endl;
 
     input_file.close();
     output_file.close();
 }
 
 
+
 int main(int argc, char* argv[]) {
     if (argc != 4) {
-        std::cout << "Enter the PATH to the input file and to the output file";
+        std::cout << "Enter the PATH to the input file, to the output file and <n>";
         return 1;
     }
 
     std::string INPUT_PATH = argv[1];
     std::string OUTPUT_PATH = argv[2];
+    std::string OUTPUTPATH_FOR_THINNING = "thinning_" + OUTPUT_PATH;
+    std::string OUTPUTPATH_FOR_AVERAGING = "averaging_" + OUTPUT_PATH;
     std::string str_num = argv[3];
     int num = std::stoi(str_num);
-    averaging_image(INPUT_PATH, OUTPUT_PATH, num);
+
+    thinning_image(INPUT_PATH, OUTPUTPATH_FOR_THINNING, num);
+    averaging_image(INPUT_PATH, OUTPUTPATH_FOR_AVERAGING, num);
 
 
     return 0;
