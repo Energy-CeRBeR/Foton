1d2f11c8f5c087c30f2f563cca96daeff5ae5f8a
Igor Chesnykh (notebook) <igor.chesnyx@mail.ru>
2024-04-09 20:41:36 +0300

    Start to create 8 task

--- Solution/T2/T2.cpp
+++ Solution/T2/T2.cpp
@@ -1,59 +1,128 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <cmath>
+#include <chrono>
 #include <vector>
 
+#pragma pack(2)
+struct BitmapFileHeader {
+    char header[2];
+    unsigned int fileSize;
+    unsigned int reserved;
+    unsigned int dataOffset;
+};
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        std::cout << "Enter the path to the file";  // Если введённый путь некорректен/отсутствует
+struct BitmapInfoHeader {
+    unsigned int size;
+    int width;
+    int height;
+    unsigned short planes;
+    unsigned short bitCount;
+    unsigned int compression;
+    unsigned int imageSize;
+    int xPixelsPerMeter;
+    int yPixelsPerMeter;
+    unsigned int colorsUsed;
+    unsigned int colorsImportant;
+};
+
+
+int main() {
+    std::string filename;
+    std::cout << "Enter the BMP image filename: ";
+    std::cin >> filename;
+
+    std::ifstream file(filename, std::ios::in | std::ios::binary);
+    if (!file) {
+        std::cerr << "Could not open file." << std::endl;
         return 1;
     }
 
-    std::ifstream input_file(argv[1], std::ios::binary);
-    std::ofstream output_file(argv[2], std::ios::binary);
+    BitmapFileHeader fileHeader;
+    BitmapInfoHeader infoHeader;
 
-    if (!(input_file.is_open() && output_file.is_open())) {
-        std::cout << "Can't open one of the files";
-        input_file.close();  // Если не удаётся получить доступ к файлу 
-        output_file.close();  // или указан неверный путь
-        return 1;
+    file.read(reinterpret_cast<char*>(&fileHeader), sizeof(BitmapFileHeader));
+    file.read(reinterpret_cast<char*>(&infoHeader), sizeof(BitmapInfoHeader));
+
+    int width = infoHeader.width;
+    int height = infoHeader.height;
+    int pixelSize = infoHeader.bitCount / 8;
+    int padding = (4 - ((width * pixelSize) % 4)) % 4;
+    int row_size = std::floor((infoHeader.bitCount * width + 31) / 32) * 4;
+    int colorsChannels = infoHeader.bitCount / 8;
+
+    file.seekg(fileHeader.dataOffset, file.beg);
+    char* pixels = new char[height * row_size + width * colorsChannels];
+    file.read(pixels, height * row_size + width * colorsChannels);
+
+    std::vector<double> mean(colorsChannels);
+    std::vector<double> variance(colorsChannels);
+
+    for (int k = 0; k < colorsChannels; k++) {
+        mean[k] = 0;
+        variance[k] = 0;
     }
-
-    char header[54];
-    input_file.read(header, 54); // Чтение служебной информации из исходного файла
-
-    if (header[0] != 'B' || header[1] != 'M') {
-        std::cout << "The source file is not a bmp format file!" << std::endl;
-        input_file.close();  // Проверка расширения исходного файла
-        output_file.close();
-        return 1;
-    }
-
-    int width = *(int*)&header[18]; // Получение значений длины и ширины файлов
-    int height = *(int*)&header[22];
-    int pixels_offset = *(int*)&header[10]; // Получение данных о смещении
-    int row_size = ((((width * 24) + 31) & ~31) >> 3); // Вычисление шага
-    std::vector<char> row(row_size);
-    char* temp_info = new char[pixels_offset - 54];
-    input_file.read(temp_info, pixels_offset - 54);
-    output_file.write(header, 54); // Запись служебной информации заголовка в новый файл
-    output_file.write(temp_info, pixels_offset - 54); // Запись информации с учётом смещения пикселей
-    delete[] temp_info;
-
-    for (int i = 0; i <= height; i++) {
-        input_file.read(row.data(), row_size); // Чтение данных из исходного файла
-        for (int j = 0; j <= width; j++) {
-            int average = 0;
-            for (int k = 0; k < 3; k++) {
-                std::swap(row[j * 3 + k], row[(width - 1 - j) * 3 + k]); // Переворачиваем изображение
+    
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            int cur_position = y * row_size + x * colorsChannels;
+            for (int k = 0; k < colorsChannels; k++) {
+                mean[k] += (int)(unsigned char)pixels[cur_position + k];
             }
         }
-        output_file.write(row.data(), row_size); // Запись пикселей в новый файл
+
+        file.seekg(padding, file.cur);
     }
 
-    std::cout << "Vertical reflection has been successfully completed!" << std::endl;
-    input_file.close();
-    output_file.close();
+    file.close();
+
+    int totalPixels = width * height;
+    for (int i = 0; i < colorsChannels; ++i) {
+        mean[i] /= totalPixels;
+    }
+
+    std::ifstream fileAgain(filename, std::ios::in | std::ios::binary);
+    fileAgain.read(reinterpret_cast<char*>(&fileHeader), sizeof(BitmapFileHeader));
+    fileAgain.read(reinterpret_cast<char*>(&infoHeader), sizeof(BitmapInfoHeader));
+    fileAgain.seekg(fileHeader.dataOffset, fileAgain.beg);
+
+    auto start = std::chrono::steady_clock::now();
+
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            int cur_position = y * row_size + x * colorsChannels;
+
+            for (int k = 0; k < colorsChannels; k++) {
+                variance[k] += ((int)(unsigned char)pixels[cur_position + k] - mean[k]) * ((int)(unsigned char)pixels[cur_position + k] - mean[k]);
+            }
+        }
+        fileAgain.seekg(padding, fileAgain.cur);
+    }
+
+    fileAgain.close();
+
+    for (int i = 0; i < colorsChannels; ++i) {
+        variance[i] /= totalPixels;
+    }
+
+    std::vector<double> stdDeviation(colorsChannels);
+    for (int i = 0; i < colorsChannels; ++i) {
+        stdDeviation[i] = std::sqrt(variance[i]);
+    }
+
+    auto end = std::chrono::steady_clock::now();
+    double elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1e6;
+
+    std::cout.setf(std::ios::fixed);
+    std::cout.precision(6);
+
+    for (int i = colorsChannels - 1; i >= 0; --i) {
+        std::cout << "Mean of " << 3 - i << " channel: " << mean[i] << std::endl;
+        std::cout << "Standard deviation of " << 3 - i << " channel: " << stdDeviation[i] << std::endl;
+        std::cout << std::endl;
+    }
+
+    std::cout << "Time taken to calculate " << elapsedTime << " seconds" << std::endl;
 
     return 0;
 }
