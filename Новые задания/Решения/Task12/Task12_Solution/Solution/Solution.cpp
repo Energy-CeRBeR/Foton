390427e278549ae5dcaddafb185e798247b57e45
Igor Chesnykh <igor.chesnyx@mail.ru>
2024-06-25 22:41:18 +0300

    Start to create new thread rotate logic in 12 task

--- Новые задания/Решения/Task12/Task12_Solution/Solution/Solution.cpp
+++ Новые задания/Решения/Task12/Task12_Solution/Solution/Solution.cpp
@@ -41,7 +41,8 @@ std::ofstream output_file;
 BITMAPFILEHEADER fileHeader;
 BITMAPINFOHEADER fileInfoHeader;
 std::vector<char> pixels;
-std::vector<char> new_pixels;
+//std::vector<char> new_pixels;
+std::vector<char> current_pixels;
 int row_size;
 int colorsChannels;
 int width;
@@ -49,6 +50,7 @@ int height;
 int outputWidth;
 int outputHeight;
 int new_row_size;
+int tile_row_size;
 int pixelsOffset;
 double radian;
 double cx;
@@ -57,11 +59,9 @@ int tile_size;
 
 
 void rotate_thread(int thread_id, int num_threads, int cur_x, int cur_y) {
-    
-    //std::vector<char> current_pixels(new_row_size * tile_size + tile_size * colorsChannels);
-
-    for (int i = cur_y + thread_id; i < std::min(cur_y + tile_size, outputHeight); i += num_threads) {
-        for (int j = cur_x; j < std::min(cur_x + tile_size, outputWidth); j++) {
+    for (int i = cur_y * tile_size + thread_id; i < std::min((cur_y + 1) * tile_size, outputHeight); i += num_threads) {
+        //std::cout << i << std::endl;
+        for (int j = cur_x * tile_size; j < std::min((cur_x + 1) * tile_size, outputWidth); j++) {
             double x = (j - cx) * std::cos(radian) - (i - cy) * std::sin(radian) + width / 2;
             double y = (j - cx) * std::sin(radian) + (i - cy) * std::cos(radian) + height / 2;
 
@@ -76,18 +76,17 @@ void rotate_thread(int thread_id, int num_threads, int cur_x, int cur_y) {
             double w11 = dx * dy;
 
             if (x >= 0 && x < width && y >= 0 && y < height) {
-
-                std::vector<uint8_t> current_pixel(colorsChannels);
-                input_file.seekg(pixelsOffset + (height > 0 ? (height - y0 - 1) : y0) * row_size + x0 * colorsChannels, std::ios::beg);
-                input_file.read(reinterpret_cast<char*>(current_pixel.data()), colorsChannels);
-
                 for (int k = 0; k < colorsChannels; k++) {
-                    std::cout << (int)(unsigned char)current_pixel[k] << std::endl;
-                    new_pixels[i * new_row_size + j * colorsChannels + k] = round(
-                        w00 * current_pixel[k] +
-                        w01 * current_pixel[k] +
-                        w10 * current_pixel[k] +
-                        w11 * current_pixel[k]
+                    if (current_pixels.size() <= (i - cur_y * tile_size) * new_row_size + (j - cur_x * tile_size) * colorsChannels + k) {
+                        std::cout << current_pixels.size() << " " << (i - cur_y * tile_size) * new_row_size + (j - cur_x * tile_size) * colorsChannels + k << std::endl;
+                    }
+                    //std::cout << i - cur_y * tile_size << " " << j - cur_x * tile_size << "\t" << y0 << " " << x0 << std::endl;
+                    //std::cout << current_pixels.size() << " " << (i - cur_y * tile_size) * new_row_size + (j - cur_x * tile_size) * colorsChannels + k << std::endl;
+                    current_pixels[(i - cur_y * tile_size) * new_row_size + (j - cur_x * tile_size) * colorsChannels + k] = round(
+                        w00 * pixels[y0 * row_size + x0 * colorsChannels + k] +
+                        w01 * pixels[y0 * row_size + x0 * colorsChannels + k] +
+                        w10 * pixels[y0 * row_size + x0 * colorsChannels + k] +
+                        w11 * pixels[y0 * row_size + x0 * colorsChannels + k]
                     );
                 }
             }
@@ -110,7 +109,7 @@ void rotate_bln(const std::string INPUT_PATH, const std::string OUTPUT_PATH, int
     input_file.read((char*)&fileHeader, sizeof(fileHeader));
     input_file.read((char*)&fileInfoHeader, sizeof(fileInfoHeader));
 
-    pixelsOffset = fileHeader.bfOffBits;
+    int pixelsOffset = fileHeader.bfOffBits;
     colorsChannels = fileInfoHeader.biBitCount / 8;
     width = fileInfoHeader.biWidth;
     height = fileInfoHeader.biHeight;
@@ -125,6 +124,7 @@ void rotate_bln(const std::string INPUT_PATH, const std::string OUTPUT_PATH, int
     fileInfoHeader.biHeight = outputHeight;
 
     new_row_size = std::floor((fileInfoHeader.biBitCount * outputWidth + 31) / 32) * 4;
+    tile_row_size = std::floor((fileInfoHeader.biBitCount * tile_size + 31) / 32) * 4;
 
     output_file.write((char*)&fileHeader, sizeof(fileHeader));
     output_file.write((char*)&fileInfoHeader, sizeof(fileInfoHeader));
@@ -134,10 +134,10 @@ void rotate_bln(const std::string INPUT_PATH, const std::string OUTPUT_PATH, int
     output_file.write(temp_info, pixelsOffset - 54);
     delete[] temp_info;
 
-    //pixels.resize(row_size * height + width * colorsChannels);
-    //input_file.read(pixels.data(), row_size * height + width * colorsChannels);
+    pixels.resize(row_size * height + width * colorsChannels);
+    input_file.read(pixels.data(), row_size * height + width * colorsChannels);
 
-    new_pixels.resize(new_row_size * outputHeight + outputWidth * colorsChannels);
+    //new_pixels.resize(new_row_size * outputHeight + outputWidth * colorsChannels);
 
     cx = outputWidth / 2;
     cy = outputHeight / 2;
@@ -145,38 +145,42 @@ void rotate_bln(const std::string INPUT_PATH, const std::string OUTPUT_PATH, int
     auto startTime = std::chrono::high_resolution_clock::now();
 
     int num_threads = std::thread::hardware_concurrency();
-
     int width_tiles_counter = outputWidth / tile_size + 1; //+ (int)(outputWidth % tile_size != 0);
     int height_tiles_counter = outputHeight / tile_size + 1; //+ (int)(outputHeight % tile_size != 0);
 
-
     std::vector<std::thread> threads;
+    current_pixels.resize(new_row_size * tile_size + tile_size * colorsChannels);
+
+    int count = 0;
+    std::cout << "Total number of tiles: " << width_tiles_counter * height_tiles_counter << std::endl;
     for (int i = 0; i < height_tiles_counter; i++) {
-        int current_y = i * tile_size;
         for (int j = 0; j < width_tiles_counter; j++) {
-            int current_x = j * tile_size;
-            for (int i = 0; i < num_threads; ++i) {
-                threads.push_back(std::thread(rotate_thread, i, num_threads, current_x, current_y));
+            for (int k = 0; k < num_threads; ++k) {
+                rotate_thread(k, num_threads, j, i);
+                //threads.push_back(std::thread(rotate_thread, k, num_threads, j, i));
             }
+            
+            //std::cout << count << std::endl;
+            output_file.write(current_pixels.data(), new_row_size * tile_size + tile_size * colorsChannels);
+            //output_file.seekp(pixelsOffset + new_row_size * (i + 1) * tile_size + (tile_size + 1) * colorsChannels);
+            count++;
+            std::cout << "Processed " << count << " tiles" << std::endl;
         }
     }
-    
-
-    for (auto& thread : threads) {
-        thread.join();
-    }
+    //for (auto& thread : threads) {
+        //thread.join();
+    //}
 
     auto endTime = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = endTime - startTime;
 
-    output_file.write(new_pixels.data(), new_row_size * outputHeight + outputWidth * colorsChannels);
+    //output_file.write(new_pixels.data(), new_row_size * outputHeight + outputWidth * colorsChannels);
 
     std::cout << "New image has been successfully created using rotate_bln for " << duration.count() << " seconds" << std::endl;
 
     input_file.close();
     output_file.close();
 }
-
 
 int main(int argc, char* argv[]) {
     if (argc < 5) {
