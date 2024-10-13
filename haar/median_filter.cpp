989d42e2005e82b1d38e02ea1ebb178a99edfd76
Igor Chesnykh <igor.chesnyx@mail.ru>
2024-10-13 17:58:47 +0300

    Modify median filter to .txt data transformation

--- haar/median_filter.cpp
+++ haar/median_filter.cpp
@@ -34,67 +34,17 @@ struct BITMAPINFOHEADER
     unsigned int biClrImportant;
 };
 
-int pixelsOffset;
 int width;
 int height;
-int new_width;
-int new_height;
 int colorsChannels;
-int row_size;
-int new_row_size;
 int k;
 
-void writeHeader(std::ifstream &input_file, std::ofstream &output_file, BITMAPFILEHEADER &fileHeader, BITMAPINFOHEADER &fileInfoHeader)
+std::vector<std::vector<std::vector<double>>> createPixelsVector(const std::vector<std::vector<std::vector<double>>> &pixels)
 {
-    output_file.write((char *)&fileHeader, sizeof(fileHeader));
-    output_file.write((char *)&fileInfoHeader, sizeof(fileInfoHeader));
+    std::vector<std::vector<std::vector<double>>> new_pixels(colorsChannels,
+                                                             std::vector<std::vector<double>>(height + k,
+                                                                                              std::vector<double>(width + k, 0)));
 
-    char *temp_info = new char[pixelsOffset - 54];
-    input_file.seekg(54);
-    input_file.read(temp_info, pixelsOffset - 54);
-    output_file.write(temp_info, pixelsOffset - 54);
-    delete[] temp_info;
-}
-
-void medianFilterSort(std::ofstream &output_file, const std::string &COMPONENT_NAME, const std::vector<char> &pixels)
-{
-    std::vector<std::shared_ptr<std::ofstream>> txt_data;
-    for (int c = 1; c <= colorsChannels; ++c)
-        txt_data.push_back(std::make_shared<std::ofstream>(COMPONENT_NAME + "_data_" + std::to_string(c) + ".txt"));
-
-    std::vector<char> new_pixels(row_size * height + width * colorsChannels);
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            std::vector<std::vector<char>> neighborhood(colorsChannels);
-            for (int a = i - k / 2; a <= i + k / 2; a++)
-                for (int b = j - k / 2; b <= j + k / 2; b++)
-                    for (int c = 0; c < colorsChannels; c++)
-                        neighborhood[c].push_back(pixels[(a + k / 2) * new_row_size + (b + k / 2) * colorsChannels + c]);
-
-            for (int c = 0; c < colorsChannels; c++)
-            {
-                std::sort(neighborhood[c].begin(), neighborhood[c].end());
-                new_pixels[i * row_size + j * colorsChannels + c] = neighborhood[c][neighborhood[c].size() / 2];
-                *txt_data[c] << (int)(unsigned char)neighborhood[c][neighborhood[c].size() / 2] << " ";
-            }
-        }
-
-        for (int c = 0; c < colorsChannels; ++c)
-            *txt_data[c] << "\n";
-    }
-
-    for (int c = 0; c < colorsChannels; ++c)
-        txt_data[c]->close();
-
-    output_file.write(new_pixels.data(), row_size * height + width * colorsChannels);
-    output_file.close();
-}
-
-std::vector<char> createPixelsVector(const std::vector<char> &pixels)
-{
-    std::vector<char> new_pixels(new_row_size * new_height + new_width * colorsChannels);
     for (int i = 0; i < height + k; i++)
     {
         for (int j = 0; j < width + k; j++)
@@ -121,7 +71,7 @@ std::vector<char> createPixelsVector(const std::vector<char> &pixels)
 
             for (int c = 0; c < colorsChannels; c++)
             {
-                new_pixels[i * new_row_size + j * colorsChannels + c] = pixels[i_2 * row_size + j_2 * colorsChannels + c];
+                new_pixels[c][i][j] = pixels[c][i_2][j_2];
             }
         }
     }
@@ -129,67 +79,99 @@ std::vector<char> createPixelsVector(const std::vector<char> &pixels)
     return new_pixels;
 }
 
-void medianFilter(const std::string INPUT_PATH, const std::string OUTPUT_PATH, const std::string &COMPONENT_NAME)
+std::vector<std::vector<double>> get_data_from_txt(const std::string PATH)
 {
-    std::ifstream input_file(INPUT_PATH, std::ios::binary);
-    if (!(input_file.is_open()))
+    std::ifstream file(PATH);
+    if (!file.is_open())
     {
-        std::cout << "Can't open input_file";
-        input_file.close();
+        std::cout << "Can't open file!\n";
         exit(1);
     }
 
-    BITMAPFILEHEADER fileHeader;
-    input_file.read((char *)&fileHeader, sizeof(fileHeader));
+    std::vector<std::vector<double>> data;
+    std::string line;
 
-    BITMAPINFOHEADER fileInfoHeader;
-    input_file.read((char *)&fileInfoHeader, sizeof(fileInfoHeader));
-
-    pixelsOffset = fileHeader.bfOffBits;
-    colorsChannels = fileInfoHeader.biBitCount / 8;
-    width = fileInfoHeader.biWidth;
-    height = fileInfoHeader.biHeight;
-    new_width = width + k;
-    new_height = height + k;
-    row_size = std::floor((fileInfoHeader.biBitCount * width + 31) / 32) * 4;
-    new_row_size = std::floor((fileInfoHeader.biBitCount * new_width + 31) / 32) * 4;
-
-    std::ofstream output_file(OUTPUT_PATH, std::ios::binary);
-    if (!(output_file.is_open()))
+    while (getline(file, line))
     {
-        std::cout << "Can't open output_file";
-        input_file.close();
-        output_file.close();
-        exit(1);
+        std::vector<double> row;
+        std::stringstream ss(line);
+        double value;
+        while (ss >> value)
+        {
+            row.push_back(value);
+        }
+        data.push_back(row);
     }
 
-    writeHeader(input_file, output_file, fileHeader, fileInfoHeader);
+    file.close();
 
-    std::vector<char> pixels(row_size * height + width * colorsChannels);
-    input_file.read(pixels.data(), row_size * height + width * colorsChannels);
-    input_file.close();
+    return data;
+}
 
-    std::vector<char> special_pixels = createPixelsVector(pixels);
-    medianFilterSort(output_file, COMPONENT_NAME, special_pixels);
+void medianFilterTXT(std::string &DIR_PATH, const std::string &COMPONENT_NAME)
+{
+    std::vector<std::vector<std::vector<double>>> input_data;
+    std::vector<std::shared_ptr<std::ofstream>> output_data;
+    for (int c = 1; c <= colorsChannels; ++c)
+    {
+        input_data.push_back(get_data_from_txt(DIR_PATH + COMPONENT_NAME + "_data_" + std::to_string(c) + ".txt"));
+        output_data.push_back(std::make_shared<std::ofstream>(COMPONENT_NAME + "_data_" + std::to_string(c) + ".txt"));
+    }
 
-    std::cout << "Median filter with std::sort was successfully applied!\n";
+    height = input_data[0].size();
+    width = input_data[0][0].size();
+
+    std::vector<std::vector<std::vector<double>>> pixels = createPixelsVector(input_data);
+    std::vector<std::vector<std::vector<double>>> new_pixels(colorsChannels,
+                                                             std::vector<std::vector<double>>(height,
+                                                                                              std::vector<double>(width, 0)));
+
+    std::cout << "Start median filter!\n";
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            std::vector<std::vector<double>> neighborhood(colorsChannels);
+            for (int a = i - k / 2; a <= i + k / 2; a++)
+                for (int b = j - k / 2; b <= j + k / 2; b++)
+                    for (int c = 0; c < colorsChannels; c++)
+                        neighborhood[c].push_back(pixels[c][a + k / 2][b + k / 2]);
+
+            for (int c = 0; c < colorsChannels; c++)
+            {
+                std::sort(neighborhood[c].begin(), neighborhood[c].end());
+                new_pixels[c][i][j] = neighborhood[c][neighborhood[c].size() / 2];
+                *output_data[c] << neighborhood[c][neighborhood[c].size() / 2] << " ";
+            }
+        }
+
+        for (int c = 0; c < colorsChannels; ++c)
+            *output_data[c] << "\n";
+    }
+
+    for (int c = 0; c < colorsChannels; ++c)
+        output_data[c]->close();
+
+    std::cout << "The application of the median filter to the text data was successful!\n";
 }
 
 int main(int argc, char *argv[])
 {
     if (argc < 5)
     {
-        std::cout << "Enter the PATH to the .bmp component, to the updated component path, component name (LL, LH, HL or HH) and the scanning window size k";
+        std::cout << "Enter the PATH to dir with .txt file, component name (LL, LH, HL or HH), num of colors channels and the scanning window size k";
         return 1;
     }
 
-    std::string INPUT_PATH = argv[1];
-    std::string OUTPUT_PATH = argv[2];
-    std::string COMPONENT_NAME = argv[3];
+    std::string DIR_PATH = argv[1];
+    std::string COMPONENT_NAME = argv[2];
+    std::string str_colors_channels = argv[3];
     std::string str_k = argv[4];
     k = std::stoi(str_k);
+    colorsChannels = std::stoi(str_colors_channels);
 
-    medianFilter(INPUT_PATH, OUTPUT_PATH, COMPONENT_NAME);
+    medianFilterTXT(DIR_PATH, COMPONENT_NAME);
 
     return 0;
 }
